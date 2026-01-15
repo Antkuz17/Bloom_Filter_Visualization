0c62fd4b14aebf587126b27ae5835aee0929a8e3
Anton Kuzmichev <Antonkuzmichev17@gmail.com>
2026-01-14 22:42:40 -0500

    setup thingy

---
+++ Logic/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, Extension
+import pybind11
+
+ext_modules = [
+    Extension(
+        'bloom',
+        ['bindings.cpp', 'bloomfilter.cpp'], 
+        include_dirs=[pybind11.get_include()],
+        language='c++',
+        extra_compile_args=['-std=c++17'],
+    ),
+]
+
+setup(
+    name='bloom',
+    version='0.1.0',
+    ext_modules=ext_modules,
+    zip_safe=False,
+)
